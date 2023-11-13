f9cb2f3a29f0c78d110bc346ae58c9b66209593a
nahum0804 <n4hummuro227@gmail.com>
2023-11-13 15:00:20 -0600

    Initial configs

---
+++ triviaQuirkProject/src/main/java/com/proyecto/triviaquirkproject/TriviaQuirkProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.triviaquirkproject;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TriviaQuirkProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
